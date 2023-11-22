509edb350be2b7067637880bd20d0ea606b639f7
tibero-yongjun <yongjun_kim2@tmax.co.kr>
2023-11-22 11:53:13 +0900

    feature: 커널 코드 실행 기능 구현

--- app/config/kernel.py
+++ app/config/kernel.py
@@ -2,8 +2,11 @@
 # -*- coding: utf-8 -*-
 # app/config/kernel.py
 
-from typing import Dict
+import asyncio
+from enum import Enum, unique
+from typing import Any, Dict, List
 
+from anyio import sleep
 from fastapi import Request
 from jupyter_client.session import Session
 from tornado import ioloop
@@ -17,19 +20,41 @@ from kernel.kernel_node import Flow, KernelNode
 settings = settings.get()
 
 
+@unique
+class Status(Enum):
+    IDLE = "idle"
+    BUSY = "busy"
+
+
 class KernelConnection(KernelNode):
+    id: str  # uuid4
     alive: bool
+    status: Status
+    executed: int
+    executing: int
+    reply: Dict[str, List[str]]
+    reply_futures: Dict[str, asyncio.Future]
 
     _process_key: bytes
     _session: Session
     _channels: Dict[str, ZMQStream]
     _pong: bool = False
+    _hb_start_handle: Any
+    _hb_handle: ioloop.PeriodicCallback
 
     def __init__(self, kernel_id, connection) -> None:
         context = super().__init__(
             NodeType.Connection,
             root_path=f"{settings.kernel_root}/{kernel_id}",
         )
+
+        self.id = kernel_id
+        self.alive = False
+        self.status = Status.IDLE
+        self.executed = 0
+        self.executing = 0
+        self.reply = {}
+        self.reply_futures = {}
 
         self._process_key = connection["process_key"].encode()
         self.connect(
@@ -54,7 +79,6 @@ class KernelConnection(KernelNode):
         for ch in ["iopub", "shell"]:
             self._channels[ch].on_recv_stream(self.on_recv_session)
 
-        self.alive = False
         self._start_hb()
 
     def _start_hb(self):
@@ -99,8 +123,57 @@ class KernelConnection(KernelNode):
         for stream in self._channels.values():
             stream.close()
 
-    def on_recv_session(self, *_):
-        pass
+    def on_recv_session(self, _, msg_list) -> None:
+        _, msg_list = self._session.feed_identities(msg_list)
+        msg = self._session.unserialize(msg_list)
+        type = msg["msg_type"]
+
+        if type == "execute_reply":
+            self.executing = msg["content"]["execution_count"]
+        elif type == "status":
+            self.status = Status(msg["content"]["execution_state"])
+
+        if "msg_id" in msg["parent_header"]:
+            id = msg["parent_header"]["msg_id"]
+            if id in self.reply:
+                if type == "stream":
+                    self.reply[id].append(msg["content"]["text"])
+                elif type == "error":
+                    self.reply[id].append("\n".join(msg["content"]["traceback"]))
+                elif type == "execute_reply":
+                    self.reply_futures[id].set_result(self.reply[id])
+
+    async def execute(self, code, msg_id: str | None = None) -> str:
+        while not self.status is Status.IDLE:
+            print(self.status)
+            await sleep(0.1)
+
+        msg = self._session.msg(
+            "execute_request",
+            {
+                "code": code,
+                "silent": False,
+                "allow_stdin": False,
+                "store_history": False,
+            },
+        )
+
+        if msg_id:
+            msg["header"]["msg_id"] = msg_id
+        else:
+            msg_id = msg["msg_id"]
+
+        self.reply[msg_id] = []
+        self.reply_futures[msg_id] = asyncio.get_running_loop().create_future()
+        self.executed += 1
+        self.executing += 1
+
+        self._session.send(self._channels["shell"], msg)
+
+        reply = await self.reply_futures[msg_id]
+        del self.reply_futures[msg_id]
+
+        return reply
 
 
 class KernelClient(KernelNode):
@@ -117,13 +190,20 @@ class KernelClient(KernelNode):
         flow.set_cleanup()
 
         kernel = KernelConnection(**connection)
+        self.kernels[kernel.id] = kernel
         flow.future.set_result(kernel)
+
         self.del_flow(flow)
 
-    async def create_kernel(self):
+    async def create_kernel(self) -> KernelConnection:
         flow = self.new_flow(future=True)
+
         self.send(ClientMessage.REQ_KERNEL, flow=flow, to_master=True)
+
         return await flow.future
+
+    def get(self, id: str) -> KernelConnection:
+        return self.kernels[id]
 
 
 def init(app) -> None:
--- app/router/kernel_router.py
+++ app/router/kernel_router.py
@@ -2,7 +2,9 @@
 # -*- coding: utf-8 -*-
 # app/routes/kernel_route.py
 
-from fastapi import APIRouter, Depends, status
+from typing import Annotated
+
+from fastapi import APIRouter, Depends, Form, HTTPException, status
 
 from app.config.kernel import KernelClient, get_client
 
@@ -11,4 +13,19 @@ router = APIRouter(prefix="/kernels", tags=["Kernel"])
 
 @router.post("/", status_code=status.HTTP_201_CREATED)
 async def create_kernel(kc: KernelClient = Depends(get_client)):
-    await kc.create_kernel()
+    kernel = await kc.create_kernel()
+
+    return kernel.id
+
+
+@router.post("/{id}")
+async def execute_command(
+    id: str, code: Annotated[str, Form()], kc: KernelClient = Depends(get_client)
+):
+    try:
+        kernel = kc.get(id)
+        result = await kernel.execute(code)
+
+        return result
+    except KeyError:
+        raise HTTPException(status_code=404, detail="kernel not found")
