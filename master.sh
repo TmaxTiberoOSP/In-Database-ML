85f09beb26036a95f5931b4024a1ee86c7fa7dfa
tibero-yongjun <yongjun_kim2@tmax.co.kr>
2023-11-23 11:05:00 +0900

    feature: 커널 서버 구동 스크립트 개선

--- master.sh
+++ master.sh
@@ -1,3 +1,7 @@
 # /bin/bash
-pkill -9 -ef kernel.kernel_master
-python -u -m kernel.kernel_master
+path=`readlink -f "${BASH_SOURCE:-$0}"`
+proj_path=`dirname $path`
+
+pkill -15 -ef kernel.kernel_master
+nohup python -u -m kernel.kernel_master > $proj_path/master.log 2>&1 &
+rm -f kernel_master; ln -s ~/.kernel_master $proj_path/kernel_master
--- provider.sh
+++ provider.sh
@@ -1,3 +1,7 @@
 # /bin/bash
-pkill -9 -ef kernel.kernel_provider
-python -u -m kernel.kernel_provider 127.0.0.1:8090
+path=`readlink -f "${BASH_SOURCE:-$0}"`
+proj_path=`dirname $path`
+
+pkill -15 -ef kernel.kernel_provider
+python -u -m kernel.kernel_provider 127.0.0.1:8090 > $proj_path/provider.log 2>&1 &
+rm -f kernel_provider; ln -s ~/.kernel_provider $proj_path/kernel_provider
