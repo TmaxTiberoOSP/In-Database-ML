0fcc4fa458738832adb9a7e95fba798b16193e1f
tibero-yongjun <yongjun_kim2@tmax.co.kr>
2023-11-28 18:18:38 +0900

    fix: jdbc_dataloader.py 코드 정리 및 수정

--- app/util/jdbc_dataloader.py
+++ app/util/jdbc_dataloader.py
@@ -1,35 +1,34 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-# utils/loader.py
+# app/util/jdbc_dataloader.py
 
-import torch
-from torch.utils.data import Dataset, Dataloader
-import jaydebeapi
-from PIL import Image
 import io
-from torchvision import transforms
+from typing import Callable
+
+import jaydebeapi
 import numpy as np
+import torch
+from PIL import Image
+from torch.utils.data import DataLoader, Dataset
+from torchvision import transforms
 
-"""
-#database information(To fix on your setting)
-tbdriver = "com.tmax.tibero.jdbc.TbDriver"
-database_url = "jdbc:tibero:thin:@<hostname>:<port>:<database_name>"
-username = "<your_username>"
-password = "<your_password>"
-jdbc_driver_jar = "/path/to/tibero7-jdbc.jar"  # Path to the Tibero JDBC driver JAR file
-"""
+get_db_connection: Callable[[], jaydebeapi.Connection]
+try:
+    get_db_connection
+except NameError:
 
-tbdriver = "com.tmax.tibero.jdbc.TbDriver"
-database_url = "jdbc:tibero:thin:@jamong-dock:9440:TAC"
-username = "sys"
-password = "tibero"
-jdbc_driver_jar = "$TB_HOME/client/lib/jar/tibero7-jdbc.jar"  # Path to the Tibero JDBC driver JAR file
-
+    def get_db_connection() -> jaydebeapi.Connection:
+        return jaydebeapi.connect(
+            "com.tmax.tibero.jdbc.TbDriver",
+            f"jdbc:tibero:thin:@127.0.0.1:8629:tibero",
+            ["tibero", "tmax"],
+            "$TB_HOME/client/lib/jar/tibero7-jdbc.jar",
+        )
 
 
 class Classification_Dataset(Dataset):
-    def __init__(self, train: bool = True, jdbc_connection, query, transform = None):
-        self.conn = jdbc_connection
+    def __init__(self, query, transform=None):
+        self.conn = get_db_connection()
         self.cursor = self.conn.cursor()
         self.query = query
         self.cursor.execute(self.query)
@@ -43,51 +42,43 @@ class Classification_Dataset(Dataset):
         record = self.data[idx]
         label, binary_data = record[0], record[1]
 
-
-        #binary data to PIL image
+        # binary data to PIL image
         image = Image.open(io.BytesIO(binary_data))
 
-        #preprocessing if necessary
+        # preprocessing if necessary
         if self.transform is not None:
-            image= = self.transform(image)
+            image = self.transform(image)
 
-        label = torch.tensor(label, dtype = torch.int)
-        image_data = torch.tensor(np.array(image), dtype=torch.float32).permute(2,0,1)
+        label = torch.tensor(label, dtype=torch.int)
+        image_data = torch.tensor(np.array(image), dtype=torch.float32).permute(2, 0, 1)
 
         return image_data, label
 
 
-#preprocessing 예시(차후 proprecssing.py로 뺄 내용)
-norm_transform = transforms.Compose([
-    transforms.ToTensor(),
-    transforms.Normalize(mean=[0.5, 0.5, 0.5], std = [0.5, 0.5, 0.5])
-])
-
-#jdbc connection
-jdbc_connection = jaydebeapi.connect(
-    tbdriver,
-    database_url,
-    [username, password],
-    jdbc_driver_jar,
+# preprocessing 예시(차후 proprecssing.py로 뺄 내용)
+norm_transform = transforms.Compose(
+    [
+        transforms.ToTensor(),
+        transforms.Normalize(mean=[0.5, 0.5, 0.5], std=[0.5, 0.5, 0.5]),
+    ]
 )
 
-train_query = "SELECT label, image_data FROM t_training_data"
-test_query = "SELECT label, image_data FROM t_test_data"
-train_dataset = Classification_Dataset(True, jdbc_connection, train_query, norm_transform)
-test_dataset = Classification_Dataset(False, jdbc_connection, test_query, norm_transform)
-
-batch_size = 10
-
-train_loader = Dataloader(
-    train_dataset,
-    batch_size = batch_size,
-    shuffle = True,
-    num_workers = 4,
+train_loader = DataLoader(
+    Classification_Dataset(
+        "SELECT {dataset.label_column_name}, {dataset.data_column_name} FROM {dataset.table_name}",
+        norm_transform,
+    ),
+    batch_size=10,
+    shuffle=True,
+    num_workers=4,
 )
 
 test_loader = DataLoader(
-    test_dataset,
-    batch_size = batch_size,
-    shuffle = False,
-    num_workers = 4,
+    Classification_Dataset(
+        "SELECT {testset.label_column_name}, {testset.data_column_name} FROM {testset.table_name}",
+        norm_transform,
+    ),
+    batch_size=10,
+    shuffle=False,
+    num_workers=4,
 )
